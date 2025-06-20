2493767a5e93c4c075b719d277a93f0db04e1674
tomasellis <tomas.sydney.ellis@gmail.com>
2025-06-20 09:53:37 -0300

    fix: .env path

--- connector-service/drizzle.config.ts
+++ connector-service/drizzle.config.ts
@@ -5,12 +5,15 @@ export default {
   schema: './src/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
-  dbCredentials: {
+  ...(process.env.PRODUCTION_DB_URL
+    ? { dbCredentials: { url: process.env.PRODUCTION_DB_URL } }
+    : {dbCredentials: {
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '5432'),
     user: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'darwint',
     ssl: process.env.NODE_ENV === 'production' ? true : false,
-  },
+  }}),
+  
 } satisfies Config; 
